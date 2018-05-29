33ed0ada8c3d65526c69ab0c513a7314375a83fd
Matt <matthewha1993@gmail.com>
2018-05-28 18:53:10 -0700

    Add left and right children as instance variables

--- algorithms/binary_tree.rb
+++ algorithms/binary_tree.rb
@@ -1,5 +1,15 @@
 class Node
-  def initialize (value, parent, children)
-    @value
+  def initialize (value, parent, left_child, right_child)
+    @value = value
+    @parent = parent
+    @left = left_child
+    @right = right_child
+  end
+
+  def in_order_traverse
+    if @value != nil
+      @parent.in_order_traverse
+
+    end
   end
 end
