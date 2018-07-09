585094bafe63385e2913d1a0045d48ef846037c8
Matt <matthewha1993@gmail.com>
2018-07-09 16:59:38 -0700

    Add traversal and root classes for Root and Leaf

--- algorithms/binary_tree.rb
+++ algorithms/binary_tree.rb
@@ -1,5 +1,10 @@
 class Node
-  def initialize (value, parent, left_child, right_child)
+  def initialize (
+    value,
+    parent,
+    left_child,
+    right_child
+  )
     @value = value
     @parent = parent
     @left = left_child
@@ -8,8 +13,28 @@ class Node
 
   def in_order_traverse
     if @value != nil
-      @parent.in_order_traverse
-
+      @left.in_order_traverse
+      puts @value
+      @right.in_order_traverse
     end
   end
+end
+
+class Root < Node
+  def initialize (
+    parent = nil,
+  )
+
+  @value = value
+  @parent = parent
+  @left = left_child
+  @right = right_child
+end
+
+class Leaf < Node
+  def initialize (
+    left_child = nil,
+    right_child = nil
+  )
+
 end
