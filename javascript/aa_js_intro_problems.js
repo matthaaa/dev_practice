afb1c88662ad4a47c5c09d26b1d3a6ba6f832d15
Matt <matthewha1993@gmail.com>
2018-07-10 12:24:11 -0700

    Add variables to method for subString

--- javascript/aa_js_intro_problems.js
+++ javascript/aa_js_intro_problems.js
@@ -1,3 +1,7 @@
 function madLib(verb, adj, noun) {
   return (`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`);
 }
+
+function isSubstring(superString, subString) {
+
+}
